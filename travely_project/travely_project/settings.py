0b862c0535acfe947bcfbe175173d85378b71cd2
yassirsalmi <salmiyassir01@gmail.com>
2024-01-20 00:28:50 +0100

    updating the travely project folder

--- travely_project/travely_project/settings.py
+++ travely_project/travely_project/settings.py
@@ -162,8 +162,7 @@ AUTH_USER_MODEL = 'travely_auth.CustomUser'
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# AUTHENTICATION_BACKENDS = [
-#     'django.contrib.auth.backends.ModelBackend',
-#     'authentification.backend.EmailBackend',  
-# ]
+
